b0a81ff17037bbb3f2e1fc5ff135bef595dc9963
ruthdgd <rinesdelgo@gmail.com>
2024-12-10 11:29:20 -0300

    union de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { Box, Heading } from "@chakra-ui/react";
+import NewList from "./newList";
+import ToDoList from "./toDoList";
+import List from "./list";
+import Buttom from "./buttom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+
+  const [filter, setFilter] = useState("all");
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = (taskName) => {
+    const newTask = { id: Date.now(), name: taskName, completed: false };
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+  };
+
+  const toggleTaskCompletion = (taskId) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const filterTasks = () => {
+    switch (filter) {
+      case "completed":
+        return tasks.filter((task) => task.completed);
+      case "incomplete":
+        return tasks.filter((task) => !task.completed);
+      default:
+        return tasks;
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box textAlign="center" p={5} bg="blue.900" color="white" minH="100vh">
+      <Heading mb={6}>Todo List</Heading>
+      <NewList addTask={addTask} />
+      <ToDoList setFilter={setFilter} />
+      <List tasks={filterTasks()} toggleTaskCompletion={toggleTaskCompletion} />
+      <Buttom />
+    </Box>
+  );
 }
 
-export default App
+export default App;
